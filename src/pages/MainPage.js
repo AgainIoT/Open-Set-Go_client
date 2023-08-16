5849502e55987bff698a2945a93c8bab1cd7c7af
sinji2102 <sinzi031012@naver.com>
2023-08-16 17:37:00 +0900

    Feat: Create Authorization #3

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,8 +2,24 @@ import styled from "styled-components";
 import { COLOR } from "../styles/color";
 import React from "react";
 
-const HomePage = () => {
-  return <div className="homePage"></div>;
-};
+function HomePage() {
+
+  const GithubButtonProps  = {
+      clientId : 'clientId',
+      redirectUrl : 'http://localhost:3000',
+      githubURL : `https://github.com/login/oauth/authorize?client_id=a92ea7bd4cc152530209&redirect_uri=http://localhost:3000/`
+  };
+
+  function handleLogin(e){
+      window.location.href = githubURL}
+  
+  return (
+      <div >
+          <button onClick={handleLogin}>
+              깃허브 로그인
+          </button>
+      </div>
+  );
+}
 
 export default HomePage;
