c758fcdd091b2e929e3578fa5ccc1702353dcbd3
hummingbbird <pop9814@kookmin.ac.kr>
2023-08-21 20:46:41 +0900

    Rename : rename Accordion to MilestoneItem #11

--- src/components/common/MilestoneItem.js
+++ src/components/common/MilestoneItem.js
@@ -9,30 +9,45 @@ import Input from "./Input";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
-// const [state, setState] = useState({
-//   title:"",
-//   dueDate:"",
-//   description: 1,
-// });
 
-export default function BasicAccordion(placeHolder) {
+
+const MilestoneItem = (placeHolder) => {
+  const [title, setTitle] = useState("");
+  const [dueDate, setdueDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [show, setShow] = useState(false);
+
+  const toggleIsShow = () => setShow(!show);
+
+  const handleAccordion = () => {
+    setShow(!show);
+    console.log(show);
+  };
+
   return (
     <div>
       <Accordion>
         <AccordionSummary
+          onClick={handleAccordion}
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion1</Typography>
+          <div>
+            {show ? (
+              <Input name="title" width="15rem" placeHolder="Title"/>
+            ):(
+              <Typography value={title}>title</Typography>
+            )}
+          </div>
         </AccordionSummary>
+
         <AccordionDetails>
           <div>
-            <Input placeHolder="Title"/>
-            <br></br>
-            <Input placeHolder="Due date (optional)"/>
+            <Input name="dueDate" placeHolder="Due date (optional)"/>
             <br></br>
             <TextField
+              name="decription"
               sx={{ width: "150ch"}}
               multiline
               rows={8}
@@ -43,5 +58,7 @@ export default function BasicAccordion(placeHolder) {
       </Accordion>
     </div>
   );
-}
+};
+
+export default MilestoneItem;
 
