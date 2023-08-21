3e5f20d133d517a79289cd592e1a6334d3b52a40
hummingbbird <pop9814@kookmin.ac.kr>
2023-08-21 22:08:37 +0900

    Feat : make AccordionDetails and control expanded event #11

--- src/components/common/MilestoneItem.js
+++ src/components/common/MilestoneItem.js
@@ -8,47 +8,58 @@ import styled from "styled-components";
 import Input from "./Input";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import { Expand } from "@mui/icons-material";
 
 
 
-const MilestoneItem = (placeHolder) => {
+const MilestoneItem = (placeHolder, sx) => {
   const [title, setTitle] = useState("");
   const [dueDate, setdueDate] = useState("");
   const [description, setDescription] = useState("");
-  const [show, setShow] = useState(false);
-
-  const toggleIsShow = () => setShow(!show);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleAccordion = () => {
-    setShow(!show);
-    console.log(show);
+    setIsOpen(!isOpen);
+    console.log(isOpen);
   };
 
   return (
     <div>
-      <Accordion>
-        <AccordionSummary
-          onClick={handleAccordion}
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <div>
-            {show ? (
-              <Input name="title" width="15rem" placeHolder="Title"/>
-            ):(
-              <Typography value={title}>title</Typography>
-            )}
-          </div>
-        </AccordionSummary>
+      <Accordion expanded={isOpen} >
+        <div>
+          {!(isOpen)? (
+            <AccordionSummary
+              onClick={handleAccordion}
+              expandIcon={<ExpandMoreIcon/>}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <div>
+                <Typography value={title}>title</Typography>
+              </div>
+            </AccordionSummary>
+          ):(
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon/>}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <div>
+                <Input name="title" placeHolder="Title"/>
+              </div>
+            </AccordionSummary>
+          )}
+        </div>
 
         <AccordionDetails>
           <div>
             <Input name="dueDate" placeHolder="Due date (optional)"/>
             <br></br>
+            <br></br>
+            <br></br>
             <TextField
               name="decription"
-              sx={{ width: "150ch"}}
+              sx={{ width: "80rem"}}
               multiline
               rows={8}
               placeholder="Description"
