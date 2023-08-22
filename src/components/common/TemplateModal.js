0e1cc8fc0dbfadb374c37aef8c107fa8c3ba8ce2
sinji2102 <sinzi031012@naver.com>
2023-08-23 03:23:06 +0900

    Design: Modal Window Design Modification #12

--- src/components/common/TemplateModal.js
+++ src/components/common/TemplateModal.js
@@ -1,38 +1,59 @@
-import styled from "styled-components";
+import * as React from "react";
+import Button from "@mui/joy/Button";
 import Modal from "@mui/joy/Modal";
-import ReactModal from "react-modal";
-import * as PropTypes from "prop-types";
+import ModalClose from "@mui/joy/ModalClose";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
 
-const MyModal = ({ isOpen, onSubmit, onCancel }) => {
-  const handleClickSubmit = () => {
-    onSubmit();
-  };
-
-  const handleClickCancel = () => {
-    onCancel();
-  };
-
+export default function TemplateModal() {
+  const [open, setOpen] = React.useState(false);
   return (
-    <TemplateContainer>
-      <ReactModal isOpen={isOpen}>
-        <div>모달 입니다.</div>
-        <div>
-          <button onClick={handleClickSubmit}>확인</button>
-          <button onClick={handleClickCancel}>취소</button>
-        </div>
-      </ReactModal></TemplateContainer>
+    <React.Fragment>
+      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+        Find Template
+      </Button>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+          }}
+        >
+          <ModalClose
+            variant="outlined"
+            sx={{
+              top: "calc(-1/4 * var(--IconButton-size))",
+              right: "calc(-1/4 * var(--IconButton-size))",
+              boxShadow: "0 2px 12px 0 rgba(0 0 0 / 0.2)",
+              borderRadius: "50%",
+              bgcolor: "background.surface",
+            }}
+          />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={1}
+          >
+            PR Template
+          </Typography>
+          <Typography id="modal-desc" textColor="text.tertiary">
+            Make sure to use <code>aria-labelledby</code> on the modal dialog with an
+            optional <code>aria-describedby</code> attribute.
+          </Typography>
+        </Sheet>
+      </Modal>
+    </React.Fragment>
   );
-};
-
-const TemplateContainer = styled.div`
-  width: 40%;
-  height: 300px;
-`;
-
-MyModal.propTypes = {
-  isOpen: PropTypes.node.isRequired,
-  onSubmit: PropTypes.node.isRequired,
-  onCancel: PropTypes.node.isRequired,
-};
-
-export default MyModal;
+}
--- src/pages/PRTemplatePage.js
+++ src/pages/PRTemplatePage.js
@@ -1,39 +1,17 @@
-import styled from "styled-components";
-import React, { useState } from "react";
-import MyModal from "../components/common/TemplateModal";
 import * as PropTypes from "prop-types";
 import MarkdownPreview from "../components/common/MarkdownPreview";
-import { Layout } from "../layout/Layout";
+import TemplateModal from "../components/common/TemplateModal";
 
 function PRTemplatePage() {
-  const [isOpen, setOpen] = useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClickSubmit = () => {
-    setOpen(false);
-  };
-
-  const handleClickCancel = () => setOpen(false);
 
   return (
     <div>
       <div>
         <MarkdownPreview />
       </div>
-      <button onClick={handleClick}>Find Template</button>
-      <MyModal
-        isOpen={isOpen}
-        onSubmit={handleClickSubmit}
-        onCancel={handleClickCancel} />
+      <TemplateModal />
     </div>
   );
 }
-
-PRTemplatePage.propTypes = {
-  isOpen: PropTypes.node.isRequired,
-};
 
 export default PRTemplatePage;
