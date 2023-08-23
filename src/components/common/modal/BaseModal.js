8b203566f7527b6ad99f3ef9ba6c979b62244744
ancy0 <tory912@gmail.com>
2023-08-23 19:35:52 +0900

    Feat : add BaseModal #10

---
+++ src/components/common/modal/BaseModal.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { COLOR } from "../../../styles/color";
+import { useRecoilState } from "recoil";
+import Modal from "@mui/material/Modal";
+
+export const BaseModal = (props) => {
+  const handleClose = () => setModalValue(false);
+
+  const [modalValue, setModalValue] = useRecoilState(props.type);
+
+  return (
+    <StBaseModal
+      keepMounted
+      open={modalValue}
+      onClose={handleClose}
+      aria-labelledby="keep-mounted-modal-title"
+      aria-describedby="keep-mounted-modal-description"
+    >
+      <ModalContainer>{props.children}</ModalContainer>
+    </StBaseModal>
+  );
+};
+
+const StBaseModal = styled(Modal)``;
+const ModalContainer = styled.div`
+  display: flex;
+  width: 60%;
+  height: 70%;
+  padding: 2rem;
+  margin-top: 1rem;
+  transform: translate(-50%, -50%);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  border-radius: 2rem;
+  background-color: ${COLOR.MAIN_WHITE};
+`;
---
+++ src/recoil/openModal.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const gitignoreOpenState = atom({
+  key: "gitignoreOpenState",
+  default: false,
+});
+
+export const issueTemplateState = atom({
+  key: "issueTemplateState",
+  defalut: false,
+});
