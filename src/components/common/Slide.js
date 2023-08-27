c9135609b8334883af9d3279af104119d9596f78
hummingbbird <pop9814@kookmin.ac.kr>
2023-08-27 21:04:26 +0900

    Feat : Create Slide component #16

---
+++ src/components/common/Slide.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default class CenterMode extends Component {
+  render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    };
+    return (
+      <div className="container">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <style>{cssstyle}</style>
+        <h2>Center Mode</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+const cssstyle = `
+.container {
+  margin: 0 auto;
+  padding: 0px 40px 40px 40px;
+  width: 400px;
+}
+h3 {
+    background: #5f9ea0;
+    color: #fff;
+    font-size: 36px;
+    line-height: 100px;
+    margin: 10px;
+    padding: 2%;
+    position: relative;
+    text-align: center;
+}
+.slick-next:before, .slick-prev:before {
+    color: #000;
+}
+.center .slick-center h3 {
+    color: #e67e22;
+    opacity: 1;
+    -ms-transform: scale(1.08);
+    transform: scale(1.08);
+}
+.center h3 {
+    transition: all .3s ease;
+}
+`;
---
+++ src/pages/LicensePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import CenterMode from "../components/common/Slide";
+function LicensePage() {
+  return (
+    <div>
+      <CenterMode/>
+    </div>
+  );
+}
+
+export default LicensePage;
