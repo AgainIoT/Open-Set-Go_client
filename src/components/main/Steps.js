ad56ed97f0fa7436fdfb168d15ddb34927eeb7e5
hummingbbird <pop9814@kookmin.ac.kr>
2023-09-06 07:47:51 +0900

    Feat : implement main page #28

--- src/components/main/Steps.js
+++ src/components/main/Steps.js
@@ -1,20 +1,22 @@
-import Step from "./Step";
+// import Step from "./Step";
 import { COLOR } from "../../styles/color.js";
+import Step1 from "./Step1";
 import Step2 from "./Step2";
+// import Step3 from "./Step3";
+// import Step4 from "./Step4";
 import { styled } from "styled-components";
+import Step5 from "./Step5.js";
 export default function Steps() {
   return (
     <StSteps className="STEPS" style={{ backgroundColor: COLOR.MAIN_BACKGROUND }}>
+      <Step1/>
       <Step2/>
-      {/* <Step step="Step2" desc="I got something to tell you..." />
-      <Step step="Step3" desc="I got something to tell you..." />
-      <Step step="Step4" desc="I got something to tell you..." />
-      <Step step="Step5" desc="I got something to tell you..." /> */}
+      <Step5/>
     </StSteps>
   );
 }
 
 const StSteps = styled.div`
   width: 100%;
-  height: 80%;
+  height: fit-content;
 `;
