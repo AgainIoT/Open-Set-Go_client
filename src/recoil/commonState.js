e4c83c83da260f91ca6e8e60c04e1eb2224987f8
ancy0 <tory912@gmail.com>
2023-09-07 13:45:06 +0900

    Comment : add comment for state #41

--- src/recoil/commonState.js
+++ src/recoil/commonState.js
@@ -1,11 +1,15 @@
 import { atom, atomFamily } from "recoil";
 
+// state about modal
 export const modalState = atomFamily({
   key: "modalState",
   default: false,
 });
 
+// state about active step
 export const activeState = atom({ key: "activeState", default: 0 });
+
+// state for checking step completion
 export const eachStepState = atomFamily({
   key: "eachStepState",
   default: false,
