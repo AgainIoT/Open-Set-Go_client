fe200c2c4cd4d8d1a6a40b605a354a87757d2ce8
hummingbbird <pop9814@kookmin.ac.kr>
2023-10-14 20:53:25 +0900

    Feat : connect modal #104

--- src/pages/IssueTemplatePage.js
+++ src/pages/IssueTemplatePage.js
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 import { COLOR } from "../styles/color";
 import React, { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { templateContent } from "../recoil/templateState";
+import { BaseModal3 } from "../components/common/modal/BaseModal3";
+import { eachStepState, modalState } from "../recoil/commonState";
 import IssueList from "../components/common/IssueList";
+import IssueModal from "../components/common/modal/IssueModal";
+
 function IssueTemplatePage() {
+  const [modalValue, setModalValue] = useRecoilState(modalState("issue"));
+  const [content, setContent] = useRecoilState(templateContent("issue"));
+
+
+  const handleOpen = () => setModalValue(true);
   return (
     <StIssueTemplatePage>
+      <BaseModal3 type={"issue"}>
+        <IssueModal type={"issue"} />
+      </BaseModal3>
       <IssueList />
-      {/* <Tmpdiv><IssueBody/></Tmpdiv> */}
     </StIssueTemplatePage>
   );
 }
