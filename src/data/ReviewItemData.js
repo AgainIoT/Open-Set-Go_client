a17cd8d82e5ed2335be563beda15e733de35ca29
ancy0 <tory912@gmail.com>
2023-10-17 14:28:53 +0900

    Feat : add data

--- src/data/ReviewItemData.js
+++ src/data/ReviewItemData.js
@@ -22,3 +22,49 @@ export const templateItem = [
     desc: "contributors can select the appropriate template when they open new issues in the repository",
   },
 ];
+
+export const communityItem = [
+  {
+    item: "issue Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "pr Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "Readme",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "CONTRIBUTING Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+];
+
+export const securityItem = [
+  {
+    item: "issue Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "pr Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "Readme",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+  {
+    item: "CONTRIBUTING Template",
+    icon: ChecklistRoundedIcon,
+    desc: "contributors can select the appropriate template when they open new issues in the repository",
+  },
+];
