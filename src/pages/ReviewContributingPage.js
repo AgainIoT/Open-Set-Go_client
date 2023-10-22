d399628df48fce9987964f030bf19ec106259102
Sinji <sinzi031012@naver.com>
2023-10-23 04:36:26 +0900

    Chore: Add setActiveState ReviewContributingPage #142

--- src/pages/ReviewContributingPage.js
+++ src/pages/ReviewContributingPage.js
@@ -1,20 +1,18 @@
 import { styled } from "styled-components";
 import React, { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { templateContent, templateMode } from "../recoil/templateState";
-import { eachStepState,modalState } from "../recoil/commonState";
+import { useRecoilState } from "recoil";
+import { eachStepState, activeState } from "../recoil/commonState";
 import { BaseModal } from "../components/common/modal/BaseModal";
 import { TemplateModal } from "../components/common/modal/TemplateModal";
 import MarkdownPreview from "../components/common/MarkdownPreview";
 
 function ReviewContributingPage(props) {
-  const [modalValue, setModalValue] = useRecoilState(modalState("contributing"));
-  const [content, setContent] = useRecoilState(templateContent("contributing"));
-  const [templateMod, setTemplateMod] = useRecoilState(templateMode);
   const [stepComplete, setStepComplted] = useRecoilState(eachStepState("5"));
+  const [activeStep, setActiveStep] = useRecoilState(activeState);
 
   useEffect(() => {
     setStepComplted(true);
+    setActiveStep(5);
   }, []);
 
   return (
