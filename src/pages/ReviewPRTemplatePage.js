d253d4d6527ac707f7b87f476b490f741237577a
Sinji <sinzi031012@naver.com>
2023-10-23 02:49:27 +0900

    Chore: Remove unused import and const in ReviewPRTemplate #145

--- src/pages/ReviewPRTemplatePage.js
+++ src/pages/ReviewPRTemplatePage.js
@@ -1,15 +1,12 @@
 import React, {  useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { BaseModal } from "../components/common/modal/BaseModal";
 import { activeState, eachStepState, modalState } from "../recoil/commonState";
 import MarkdownPreview from "../components/common/MarkdownPreview";
 import { TemplateModal } from "../components/common/modal/TemplateModal";
 import { styled } from "styled-components";
-import { ReviewLayout } from "../layout/ReviewLayout";
 
 function ReviewPRTemplatePage() {
-  const [modalValue, setModalValue] = useRecoilState(modalState("pr"));
-
   const [stepComplete, setStepComplted] = useRecoilState(eachStepState("3"));
   const [activeStep, setActiveStep] = useRecoilState(activeState);
 
