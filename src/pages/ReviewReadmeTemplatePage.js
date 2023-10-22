53f94219a315851ad067b56746957c605229e6f0
Sinji <sinzi031012@naver.com>
2023-10-23 05:21:48 +0900

    Refactor: Remove unused assignments and import in ReviewReadmeTemplatePage #142

--- src/pages/ReviewReadmeTemplatePage.js
+++ src/pages/ReviewReadmeTemplatePage.js
@@ -1,15 +1,12 @@
 import { styled } from "styled-components";
-import React, { useState, useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { eachStepState, modalState, activeState } from "../recoil/commonState";
-import { templateContent } from "../recoil/templateState";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { eachStepState, activeState } from "../recoil/commonState";
 import { BaseModal } from "../components/common/modal/BaseModal";
 import { TemplateModal } from "../components/common/modal/TemplateModal";
 import MarkdownPreview from "../components/common/MarkdownPreview";
 
 function ReviewReadmeTemplatePage() {
-  const [modalValue, setModalValue] = useRecoilState(modalState("readme"));
-  const [content, setContent] = useRecoilState(templateContent("readme"));
   const [stepComplete, setStepComplted] = useRecoilState(eachStepState("6"));
   const [activeStep, setActiveStep] = useRecoilState(activeState);
 
@@ -17,8 +14,6 @@ function ReviewReadmeTemplatePage() {
     setStepComplted(true);
     setActiveStep(6);
   }, []);
-
-  const handleOpen = () => setModalValue(true);
 
   return (
     <StReadmeTemplatePage>
