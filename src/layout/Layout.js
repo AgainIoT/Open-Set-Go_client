2da3aba150dd8cf9ccfb4d43f7c0c6d6bb09ee1a
bentshrimp <bent_shrimp@kookmin.ac.kr>
2023-10-23 01:27:43 +0900

    Update : step1 page is more responsible #143
    Signed-off-by: bentshrimp <bent_shrimp@kookmin.ac.kr>

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -74,8 +74,8 @@ export const Layout = () => {
 const StLayout = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: ${COLOR.MAIN_WHITE};
   overflow-x: hidden;
   overflow-y: hidden;
@@ -83,10 +83,8 @@ const StLayout = styled.div`
 
 const ContentsContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  height: 100%;
   padding: 2rem;
   margin-left: 2.6rem;
   border-top-left-radius: 2rem;
@@ -97,7 +95,7 @@ const StepContainer = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
-  height: 76vh;
+  height: 100%;
   gap: 2.5rem;
 `;
 
@@ -105,8 +103,8 @@ const ExplainWrapper = styled.div`
   display: flex;
   justify-content: center;
   width: 20%;
+  height: 100%;
   min-width: 22rem;
-  height: 100vh;
 `;
 
 const StepContentsContainer = styled.div`
@@ -130,7 +128,7 @@ const BottomContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
-  padding: 2rem 3rem 0rem 3rem;
+  padding: 2rem 2rem 0rem 2rem;
   background-color: ${COLOR.MAIN_BACKGROUND};
 `;
 
