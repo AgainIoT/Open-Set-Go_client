1d0b60d2a311fc001a4b46a6b9e679f59934fe0c
ymw0407 <yunminwo1211@gmail.com>
2023-11-20 04:11:03 +0900

    Feat : linter & formatter setting
    Signed-off-by: ymw0407 <yunminwo1211@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/constants/metadata/metadata.ts
+++ src/constants/metadata/metadata.ts
@@ -1,41 +1,41 @@
-import { Metadata } from "next";
-import { authors } from "./authors";
-import { mainPageKeywords } from "./keywords";
-import { Robots } from "next/dist/lib/metadata/types/metadata-types";
+import { Metadata } from 'next';
+import { authors } from './authors';
+import { mainPageKeywords } from './keywords';
+import { Robots } from 'next/dist/lib/metadata/types/metadata-types';
 
 /** Metadata commonly used on all pages */
 const constMetadata: Metadata = {
-  metadataBase: new URL("https://www.open-set-go.com"),
+  metadataBase: new URL('https://www.open-set-go.com'),
   description: `Best option to open your project, set it open-source friendly, and go programming. Mange your opensource project with Open-Source Project Management Platform, "Open-Set-Go"`,
-  applicationName: "Open-Set-Go",
+  applicationName: 'Open-Set-Go',
   authors,
-  generator: "Next.js",
+  generator: 'Next.js',
   keywords: mainPageKeywords,
-  referrer: "origin-when-cross-origin",
-  creator: "Team AgainIoT",
-  publisher: "Netlify",
+  referrer: 'origin-when-cross-origin',
+  creator: 'Team AgainIoT',
+  publisher: 'Netlify',
   icons: {
-    icon: [{ url: "/favicon.ico", sizes: "32" }],
-    apple: "/favicon/apple-icon.png",
+    icon: [{ url: '/favicon.ico', sizes: '32' }],
+    apple: '/favicon/apple-icon.png',
     other: {
-      rel: "apple-touch-icon-precomposed",
-      url: "/favicon/apple-icon-precomposed.png",
+      rel: 'apple-touch-icon-precomposed',
+      url: '/favicon/apple-icon-precomposed.png',
     },
   },
   openGraph: {
-    type: "website",
-    url: "https://www.open-set-go.com",
-    title: "Open-Set-Go",
+    type: 'website',
+    url: 'https://www.open-set-go.com',
+    title: 'Open-Set-Go',
     description: `Open-Source Project Management Platform ,"Open-Set-Go"`,
-    siteName: "Open-Set-Go",
-    images: "https://open-set-go.com/favicon/logo.svg",
-    locale: "en_US",
+    siteName: 'Open-Set-Go',
+    images: 'https://open-set-go.com/favicon/logo.svg',
+    locale: 'en_US',
   },
   twitter: {
-    card: "summary_large_image",
+    card: 'summary_large_image',
     description: `Open-Source Project Management Platform ,"Open-Set-Go"`,
-    images: "https://open-set-go.com/favicon/logo.svg",
-    title: "Open-Set-Go",
+    images: 'https://open-set-go.com/favicon/logo.svg',
+    title: 'Open-Set-Go',
   },
   // alternates: { // if i18n add, this will need!
   //   canonical: "/",
